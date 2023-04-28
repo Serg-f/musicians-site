e80dce131d4300b7469e7f22812ab851e686718c
Serg-f <feelwork2077@gmail.com>
2023-04-28 22:15:20 +0200

    Delete musicians_site directory

--- musicians_site/musicians/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MusiciansConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'musicians'
-    verbose_name = 'Музыканты и стили'
