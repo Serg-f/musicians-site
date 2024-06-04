2c142070891c775965ccc3a729c0e1ef2e5967ac
serg <feelwork2077@gmail.com>
2024-06-04 17:35:43 +0200

    start building microservices

--- musicians/nav_menu.py
+++
@@ -1,6 +0,0 @@
-menu = [
-    {'title': 'Musicians', 'url_name': 'musicians:home'},
-    {'title': 'Create Article', 'url_name': 'musicians:article_create'},
-    {'title': 'Contact Us', 'url_name': 'users:contact'},
-    {'title': 'About', 'url_name': 'musicians:about'},
-]
