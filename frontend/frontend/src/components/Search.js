d5e80530e9fe089e66fcb155b3e484c5dc3d7b0f
serg <feelwork2077@gmail.com>
2024-06-27 13:54:20 +0200

    search min length restriction implemented

--- frontend/frontend/src/components/Search.js
+++ frontend/frontend/src/components/Search.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
-import {Form, FormControl, Button, Row, Col} from 'react-bootstrap';
-import {useSearchParams} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Form, FormControl, Button, Row, Col } from 'react-bootstrap';
+import { useSearchParams } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Search = () => {
     const [searchTerm, setSearchTerm] = useState('');
@@ -12,7 +14,11 @@ const Search = () => {
 
     const handleSearchSubmit = (event) => {
         event.preventDefault();
-        const params = {page: 1, 'page-size': searchParams.get('page-size') || 3};
+        if (searchTerm.length < 3) {
+            toast.error('Search term must be at least 3 characters long.');
+            return;
+        }
+        const params = { page: 1, 'page-size': searchParams.get('page-size') || 3 };
         if (searchTerm) {
             params['search'] = searchTerm;
         }
@@ -20,22 +26,25 @@ const Search = () => {
     };
 
     return (
-        <Form onSubmit={handleSearchSubmit}>
-            <Row>
-                <Col>
-                    <FormControl
-                        type="text"
-                        placeholder="Search articles"
-                        className="mr-sm-2"
-                        value={searchTerm}
-                        onChange={handleSearchChange}
-                    />
-                </Col>
-                <Col xs="auto">
-                    <Button variant="outline-success" type="submit">Search</Button>
-                </Col>
-            </Row>
-        </Form>
+        <>
+            <ToastContainer />
+            <Form onSubmit={handleSearchSubmit}>
+                <Row>
+                    <Col>
+                        <FormControl
+                            type="text"
+                            placeholder="Search articles"
+                            className="mr-sm-2"
+                            value={searchTerm}
+                            onChange={handleSearchChange}
+                        />
+                    </Col>
+                    <Col xs="auto">
+                        <Button variant="outline-success" type="submit">Search</Button>
+                    </Col>
+                </Row>
+            </Form>
+        </>
     );
 };
 
