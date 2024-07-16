4140a1aab1624a8e1b584fff9ee2546e7b8dd9f5
serg <feelwork2077@gmail.com>
2024-07-16 18:38:51 +0200

    fix

--- frontend/frontend/src/context/serviceUrls.js
+++ frontend/frontend/src/context/serviceUrls.js
@@ -1,5 +1,8 @@
 // src/context/serviceUrls.js
-const usersServiceURL = process.env.REACT_APP_USERS_SERVICE_URL;
-const musiciansServiceURL = process.env.REACT_APP_MUSICIANS_SERVICE_URL;
+// const usersServiceURL = process.env.REACT_APP_USERS_SERVICE_URL;
+// const musiciansServiceURL = process.env.REACT_APP_MUSICIANS_SERVICE_URL;
+
+const usersServiceURL = 'http://localhost:8020';
+const musiciansServiceURL = 'http://localhost:8000';
 
 export { usersServiceURL, musiciansServiceURL };
