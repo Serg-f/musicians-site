841e67171ee2f07e54a150863fe74c0fd010cb26
serg <feelwork2077@gmail.com>
2024-07-25 17:57:34 +0200

    backend services urls fix

--- frontend/frontend/src/context/serviceUrls.js
+++ frontend/frontend/src/context/serviceUrls.js
@@ -1,5 +1,5 @@
 // src/context/serviceUrls.js
-const usersServiceURL = process.env.REACT_APP_USERS_SERVICE_URL || 'http://localhost:8020';
-const musiciansServiceURL = process.env.REACT_APP_MUSICIANS_SERVICE_URL || 'http://localhost:8000';
+const usersServiceURL = process.env.REACT_APP_USERS_SERVICE_URL || 'https://users-service-2d4imkwuza-ey.a.run.app';
+const musiciansServiceURL = process.env.REACT_APP_MUSICIANS_SERVICE_URL || 'https://musicians-service-2d4imkwuza-ey.a.run.app';
 
 export { usersServiceURL, musiciansServiceURL };
