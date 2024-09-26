d07a632c3b856772a0400e7f5abcb3fc0d7d7507
serg <feelwork2077@gmail.com>
2024-09-26 17:49:02 +0200

    message broker works

--- users/src/users_app/management/commands/consume_user_stats.py
+++ users/src/users_app/management/commands/consume_user_stats.py
@@ -1,3 +1,4 @@
+import time
 import pika
 import json
 from django.core.management.base import BaseCommand
@@ -7,25 +8,42 @@ class Command(BaseCommand):
     help = 'Consume messages from the user_statistics queue'
 
     def handle(self, *args, **kwargs):
-        connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
-        channel = connection.channel()
-        channel.queue_declare(queue='user_statistics')
-
-        def callback(ch, method, properties, body):
-            message = json.loads(body)
-            user_id = message['user_id']
-            articles_published = message['articles_published']
-            articles_total = message['articles_total']
-
+        retries = 5
+        while retries > 0:
             try:
-                user = CustomUser.objects.get(id=user_id)
-                user.articles_published = articles_published
-                user.articles_total = articles_total
-                user.save()
-                self.stdout.write(self.style.SUCCESS(f'Successfully updated stats for user {user_id}'))
-            except CustomUser.DoesNotExist:
-                self.stdout.write(self.style.ERROR(f'User {user_id} does not exist'))
+                connection = pika.BlockingConnection(
+                    pika.ConnectionParameters(
+                        host='rabbitmq',
+                        port=5672,
+                        connection_attempts=5,
+                        retry_delay=5
+                    )
+                )
+                channel = connection.channel()
+                channel.queue_declare(queue='user_statistics')
 
-        channel.basic_consume(queue='user_statistics', on_message_callback=callback, auto_ack=True)
-        self.stdout.write(self.style.SUCCESS('Starting to consume messages from user_statistics queue'))
-        channel.start_consuming()
+                def callback(ch, method, properties, body):
+                    message = json.loads(body)
+                    user_id = message['user_id']
+                    articles_published = message['articles_published']
+                    articles_total = message['articles_total']
+
+                    try:
+                        user = CustomUser.objects.get(id=user_id)
+                        user.articles_published = articles_published
+                        user.articles_total = articles_total
+                        user.save()
+                        self.stdout.write(self.style.SUCCESS(f'Successfully updated stats for user {user_id}'))
+                    except CustomUser.DoesNotExist:
+                        self.stdout.write(self.style.ERROR(f'User {user_id} does not exist'))
+
+                channel.basic_consume(queue='user_statistics', on_message_callback=callback, auto_ack=True)
+                self.stdout.write(self.style.SUCCESS('Starting to consume messages from user_statistics queue'))
+                channel.start_consuming()
+                break
+            except pika.exceptions.AMQPConnectionError as e:
+                self.stdout.write(self.style.ERROR(f'Connection failed: {e}'))
+                retries -= 1
+                time.sleep(5)
+        if retries == 0:
+            self.stdout.write(self.style.ERROR('Failed to connect to RabbitMQ after several attempts'))
