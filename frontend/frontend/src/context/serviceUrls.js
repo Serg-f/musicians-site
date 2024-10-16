3554bd852baa0d7aeb66ca25f6c78248dea6ed6b
serg <feelwork2077@gmail.com>
2024-10-16 16:37:30 +0200

    frontend --build-arg API_URL implemented

--- frontend/frontend/src/context/serviceUrls.js
+++ frontend/frontend/src/context/serviceUrls.js
@@ -1,5 +1,5 @@
 // src/context/serviceUrls.js
-const apiGatewayURL = process.env.REACT_APP_API_GATEWAY_URL || 'http://localhost:8000';
+const apiGatewayURL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 const usersServiceURL = `${apiGatewayURL}/users`;
 const musiciansServiceURL = `${apiGatewayURL}/musicians`;
